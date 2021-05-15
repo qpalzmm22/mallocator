697f0b8b50636187ba8fc0f5dfbd73d1b1ad5ab2
qpalzmm22 <qpalzmm22@gmail.com>
2021-05-15 12:03:02 +0900

    laptop2 desktop

---
+++ rmalloc copy 2.c
@@ -0,0 +1,207 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include "rmalloc.h" 
+
+
+static rm_header rm_free_list = { 0x0, 0 } ;
+static rm_header rm_used_list = { 0x0, 0 } ;
+
+rm_header_ptr p_prev_free_node = &rm_free_list; // points to last node on the free_list
+rm_header_ptr p_prev_used_node = &rm_used_list;	// points to last node on the used_list
+
+
+
+// v_addr->next = null, v_addr->size = s
+// returns the ptr of the v_addr. 
+rm_header_ptr create_node(void * v_addr, size_t s){
+	printf("(create_node) Creating node at %p with size %ld\n", v_addr, s);
+	rm_header_ptr newnode = (rm_header_ptr) v_addr;
+	newnode->next = 0x0;
+	newnode->size = s;
+
+	return newnode;
+}
+
+rm_header_ptr insert_node(rm_header_ptr prev_node, void * v_addr, size_t s){
+	rm_header_ptr curr_node = prev_node->next;
+	rm_header_ptr new_node = create_node(v_addr, curr_node->size - s + sizeof(rm_header));
+	
+	new_node->next = curr_node->next;
+	prev_node->next = new_node;
+
+	return new_node;
+}
+
+
+// mmap more size by pgsize
+void call_more_page(size_t pg_size, rm_header_ptr end_node){
+	printf("Calling more pages(%ld) \n", pg_size);
+	void * pg_front = mmap(0x0, pg_size / sysconf(_SC_PAGE_SIZE) + sysconf(_SC_PAGE_SIZE), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+
+	// link to last node of free_list
+	end_node->next = create_node(pg_front, pg_size - sizeof(rm_header) );
+
+}
+
+void free_to_used(rm_header_ptr free, rm_header_ptr used){
+	used->next = free;
+}
+void * rmalloc (size_t s) 
+{
+	void *pg_free;
+
+	// Initilize first page, bring pages in lazy manner.
+	if(rm_free_list.next == 0x0){
+		printf("Initialize free list\n");
+		size_t pgsize = sysconf(_SC_PAGE_SIZE);
+
+		pg_free = mmap(0x0, pgsize, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+		printf("init at %p\n", pg_free);
+		rm_free_list.next = create_node( pg_free, pgsize - sizeof(rm_header));
+	}
+
+	// Allocate s size on free list with iterating
+	for(rm_header_ptr iter = rm_free_list.next ; iter != 0x0 ; iter = iter->next){
+		printf("Iterating free list at :%p, size : %ld\n", iter, iter->size);
+
+		// consider two cases : 1. no page available 2. no v_size found. What to consider first ...
+		
+		// if size == s
+		if(s == iter->size){
+			printf("size == request\n");
+			// switch free to used 
+			p_prev_used_node->next = iter;
+			p_prev_used_node->next->size = s;
+
+			// remove from free list
+			if(iter->next == 0x0){
+			    //free(zeroize the area)
+				
+				p_prev_free_node->next = 0x0;
+			}
+			else{
+				p_prev_free_node->next = iter->next;
+				//free(zeroize the area)
+			}
+			break;
+		}
+
+		// if s < size
+		if(s < iter->size){
+			printf("request < size at %p\n", iter);
+			printf("iter->size %ld: \n", iter->size);
+			
+			// insert at the end		
+			p_prev_used_node->next = iter;
+			p_prev_used_node->next->size = s;
+
+			// reduce the size by s + sizeof(node) and insert.
+			return insert_node(p_prev_free_node, iter + sizeof(rm_header) + iter->size , iter->size - s);
+		}
+
+		// if s > pgsize and not found in iteration
+		if(iter->next == 0x0){
+			call_more_page(s, iter);
+		}
+
+		// recording previous free node
+		p_prev_free_node = iter;
+	}
+
+	
+	// first fit
+	// iterate free_list and find a first free space and allocate area 
+
+	return p_prev_free_node->next ; // erase this
+}
+
+void rfree (void * p) 
+{
+	// TODO 
+}
+
+void * rrealloc (void * p, size_t s) 
+{
+	void * p_orig; 
+	// iterate and find p in used_list
+	for(rm_header_ptr u_iter = rm_used_list.next ; u_iter != 0x0 ; u_iter = u_iter->next){
+		
+		// if p is found
+		if(p == u_iter){
+			for(rm_header_ptr f_iter = rm_free_list.next ; f_iter != 0x0 ; f_iter = f_iter->next){
+				p + sizeof(rm_header) + s;
+				// if(){
+
+				// }
+			// check if reallocatable (iterate free list and find if (p + sizeof(rm_header + size)) is between(free_add - size , free_add))
+			break;
+			}
+		}
+
+		
+		// if not found, just alloc			
+		if(u_iter->next == 0x0);
+		
+	}
+
+	// if size == 0 just allocate with 0 address
+	if(s == 0){
+		// remove from
+		return NULL;
+	}
+
+	return 0x0 ; 
+}
+
+void rmshrink () 
+{
+	// TODO
+}
+
+void rmconfig (rm_option opt) 
+{
+	// TODO
+}
+
+// int main(){
+// 	//dont use this one
+// 	rmalloc(10);
+// 	rmprint();
+// 	rmalloc(100);
+// 	rmprint();
+// 	rmalloc(2000);
+// 	rmprint();
+// }
+
+
+void 
+rmprint () 
+{
+	rm_header_ptr itr ;
+	int i ;
+
+	printf("==================== rm_free_list ====================\n") ;
+	for (itr = rm_free_list.next, i = 0 ; itr != 0x0 ; itr = itr->next, i++) {
+		printf("%3d:%p:%8d:", i, ((void *) itr) + sizeof(rm_header), (int) itr->size) ;
+
+		int j ;
+		char * s = ((char *) itr) + sizeof(rm_header) ;
+		for (j = 0 ; j < (itr->size >= 8 ? 8 : itr->size) ; j++) 
+			printf("%02x ", s[j]) ;
+		printf("\n") ;
+	}
+	printf("\n") ;
+
+	printf("==================== rm_used_list ====================\n") ;
+	for (itr = rm_used_list.next, i = 0 ; itr != 0x0 ; itr = itr->next, i++) {
+		printf("%3d:%p:%8d:", i, ((void *) itr) + sizeof(rm_header), (int) itr->size) ;
+
+		int j ;
+		char * s = ((char *) itr) + sizeof(rm_header) ;
+		for (j = 0 ; j < (itr->size >= 8 ? 8 : itr->size) ; j++) 
+			printf("%02x ", s[j]) ;
+		printf("\n") ;
+	}
+	printf("\n") ;
+}
--- rmalloc.c
+++ rmalloc.c
@@ -4,118 +4,100 @@
 #include "rmalloc.h" 
 
 
-rm_header rm_free_list = { 0x0, 0 } ;
-rm_header rm_used_list = { 0x0, 0 } ;
+static rm_header rm_free_list = { 0x0, 0 } ;
+static rm_header rm_used_list = { 0x0, 0 } ;
 
-rm_option g_opt = FirstFit;
-
-int* pg_free = 0x0;
-int* p_free = 0x0;
-int* p_used = 0x0;
-
-/* 
- * USE mmap to allocate memory by page
- * 1. first fit
- * 2. best fit
- * 3. worst fit 
- */
+rm_header_ptr p_prev_free_node = &rm_free_list; // points to last node on the free_list
+rm_header_ptr p_last_used_node = &rm_used_list;	// points to last node on the used_list
 
 
-rm_header* create_node(void * v_addr, size_t s){
-	printf("Creating node at %p with size %ld\n", v_addr, s);
-	rm_header * newfree = (rm_header *)v_addr;
-	newfree->size = s;
-	newfree->next = 0x0;
 
-	return newfree;
+// v_addr->next = null, v_addr->size = s
+// returns the ptr of the v_addr. 
+rm_header_ptr create_node(void * v_addr, size_t s){
+	printf("(create_node) Creating node at %p with size %ld\n", v_addr, s);
+	rm_header_ptr newnode = (rm_header_ptr) v_addr;
+	newnode->next = 0x0;
+	newnode->size = s;
+
+	return newnode;
 }
 
-
-void * rmalloc (size_t s) 
+// size section of rm_header 
+static 
+void * 
+_data (rm_header_ptr e)
 {
-	int *mapped = NULL;
-	long int a=0;
-	int* newmapped = 0x0;
-	newmapped = mmap(0x0, 50, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	printf("p: %p : %ld\n", newmapped,a++);
-	newmapped = mmap(0x0, sysconf(_SC_PAGE_SIZE), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	printf("p: %p : %ld\n", newmapped,a++);
-	newmapped = mmap(0x0, sysconf(_SC_PAGE_SIZE)+2, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	printf("p: %p : %ld\n", newmapped,a++);
-	newmapped = mmap(0x0, sysconf(_SC_PAGE_SIZE)+3, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	printf("p: %p : %ld\n", newmapped,a++);
-	printf("p: %p\n", newmapped);
-	printf("p: %p\n",NULL);
+	return ((void *) e) + sizeof(rm_header) ;
+}
 
-	static rm_header * lst_used_node = 0x0;
-	rm_header * prev_free_node;
+static 
+void 
+sm_container_split (rm_header_ptr prev_hole, size_t size)
+{
+	rm_header_ptr hole = prev_hole->next;
+	// remainder address
+	rm_header_ptr remainder = (rm_header_ptr) (_data(hole) + size) ;
 
-	// Initilize first page, bring pages in lazy manner.
-	if(rm_free_list.next == 0x0){
-		printf("Initialize free list\n");
-		pg_free = mmap(0x0, sysconf(_SC_PAGE_SIZE), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-		
-		rm_free_list.next = create_node(pg_free + sysconf(_SC_PAGE_SIZE) - sizeof(rm_header), sysconf(_SC_PAGE_SIZE) - sizeof(rm_header));
-		rm_free_list.size = sysconf(_SC_PAGE_SIZE);
+	// (free list)
+	remainder->size = hole->size - size - sizeof(rm_header) ;
+	remainder->next = hole->next ;
 
-		// initialize prev_free_node
-		prev_free_node = &rm_free_list;
+	// (used list)
+	hole->next = rm_used_list.next;
+	hole->size = size ;
+	rm_used_list.next = hole;
+}
 
-	}
+static 
+void * 
+retain_more_memory (size_t size)
+{
+	rm_header_ptr hole ;
+	size_t pagesize = getpagesize() ;
+	size_t n_pages = 0;
 
-	// Allocate s size on free list with iterating
-	for(rm_header * iter = rm_free_list.next; iter != 0x0; iter = iter->next){
-		printf("Iterating free list at :%p, size : %ld\n", iter, iter->size);
-
-		// consider two cases : 1. no page available 2. no v_size found. What to consider first ...
-		
-		// if size == s
-		if(s == iter->size){
-			printf("size == request\n");
-			// switch free to used and add it at the end of the list
-			lst_used_node->next = create_node(iter + iter->size, s);
-			lst_used_node = lst_used_node->next;
-
-			// remove from free list
-			if(iter->next == 0x0){
-			    //free(zeroize the area)
-
-				prev_free_node = 0x0;
-			}
-			else{
-				prev_free_node->next = iter->next;
-				//free(zeroize the area)
-			}
-		}
-
-		// if s < size
-		if(s < iter->size){
-			printf("request < size %p\n", iter );
-			
-			lst_used_node->next = create_node(iter, s);
-			printf("1");
-			lst_used_node = lst_used_node->next;
-			printf("2");
-			// reduce the size by s + sizeof(node)
-			iter->size = iter->size - (s + sizeof(rm_header));
-			printf("3");
-		}
-
-		// if s > pgsize
-
-
-		// recording previous free node
-		// if(iter->next != 0x0){
-			
-		// }
-		prev_free_node = iter;
-	}
-
+	n_pages = (sizeof(rm_header) + size ) / pagesize  + 1 ;
+	hole = (rm_header_ptr) mmap(0x0, n_pages *n_pages, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 	
-	// first fit
-	// iterate free_list and find a first free space and allocate area 
+	if (hole == 0x0)
+		return 0x0 ;
+	
+	hole->size = n_pages * pagesize - sizeof(rm_header) ;
 
-	return 0x0 ; // erase this
+	return hole ;
+}
+
+void * 
+rmalloc (size_t size) 
+{
+	rm_header_ptr hole = 0x0, itr = 0x0 ;
+	rm_header_ptr prev_hole = &rm_free_list;
+
+	for (itr = rm_free_list.next ; itr != 0x0 ; itr = itr->next) {
+		
+		// free list fits or smaller
+		if ((itr->size == size) || (size + sizeof(rm_header) < itr->size)) {
+			hole = itr ;
+			break ; 
+		}
+		prev_hole = itr;
+	}
+
+	// if couldn't find a hole
+	if (hole == 0x0) {
+		hole = retain_more_memory(size) ;
+		if (hole == 0x0)
+			return 0x0 ;
+		
+		hole->next = rm_free_list.next ;
+		rm_free_list.next = hole;
+	}
+	//if hole is found
+	if (size < hole->size)
+		// split the area
+		sm_container_split(prev_hole, size) ;
+	return _data(hole) ;
 }
 
 void rfree (void * p) 
@@ -123,10 +105,39 @@ void rfree (void * p)
 	// TODO 
 }
 
+
+
 void * rrealloc (void * p, size_t s) 
 {
-	// TODO
-	return 0x0 ; // erase this 
+	void * p_orig; 
+	// iterate and find p in used_list
+	for(rm_header_ptr u_iter = rm_used_list.next ; u_iter != 0x0 ; u_iter = u_iter->next){
+		
+		// if p is found
+		if(p == u_iter){
+			for(rm_header_ptr f_iter = rm_free_list.next ; f_iter != 0x0 ; f_iter = f_iter->next){
+				p + sizeof(rm_header) + s;
+				// if(){
+
+				// }
+			// check if reallocatable (iterate free list and find if (p + sizeof(rm_header + size)) is between(free_add - size , free_add))
+			break;
+			}
+		}
+
+		
+		// if not found, just alloc			
+		if(u_iter->next == 0x0);
+		
+	}
+
+	// if size == 0 just allocate with 0 address
+	if(s == 0){
+		// remove from
+		return NULL;
+	}
+
+	return 0x0 ; 
 }
 
 void rmshrink () 
@@ -136,17 +147,18 @@ void rmshrink ()
 
 void rmconfig (rm_option opt) 
 {
-	
+	// TODO
 }
 
-int main(int argc, char** argv)
-{
-	rmalloc(10);
-	rmprint();
-	rmalloc(20);
-	rmprint();
-	return 0;
-}
+// int main(){
+// 	//dont use this one
+// 	rmalloc(10);
+// 	rmprint();
+// 	rmalloc(100);
+// 	rmprint();
+// 	rmalloc(2000);
+// 	rmprint();
+// }
 
 
 void 
